5d344b038f6ffebbcd3d6a516765b681734549c8
itachi-jpg <beratt.d13@gmail.com>
2022-11-22 21:30:24 +0000

    hamburger menu fully working

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 // Hamburger Menu
-const hamburger = document.querySelector(
-  ".header .nav-bar .nav-list .hamburger"
-);
+const hamburger = document.getElementById("hamburger");
 const mobile_menu = document.querySelector(".header .nav-list ul");
-
-const menu_item = document.querySelector(".header .nav-list ul li a");
-
 function active() {
   mobile_menu.classList.toggle("active");
 }
@@ -17,9 +12,20 @@ function toggleTheme() {
 }
 
 // closes hamburger menu upon link click
+const menu_item = document.querySelectorAll(".anchor");
 menu_item.forEach((item) => {
   item.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     mobile_menu.classList.toggle("active");
   });
+});
+
+// Scroll effect for navbar background
+document.addEventListener("scroll", () => {
+  let scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "#262931";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
 });
